6e2ae033481d48702b0c3086cbfec04c39c4501f
alfaizkhan <42858836+alfaiz12@users.noreply.github.com>
2021-08-18 08:41:13 +0530

    Add files via upload

---
+++ bookStore/bookStore/AfterCategories.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace bookStore
+{
+    class AfterCategories
+    {
+
+        int n = 0;
+        Categories obj = new Categories();
+        public void book_Details(int choice)
+        {
+            if (choice != 0)
+            {
+                int i = 1;
+                //JSON CODE
+                var jsonString = File.ReadAllText(@"customer_List.json");
+                var jObject = JObject.Parse(jsonString);
+                JArray booksArrary = (JArray)jObject["bookDetails"];
+                foreach (var x in booksArrary)
+                {
+                    if (x["book_Category_id"].Value<int>() == choice)
+                    {
+                        Console.WriteLine($"                          Category - {x["book_Category"].Value<string>()}\n");
+                        break;
+                    }
+                }
+
+                Console.WriteLine(" ID  |         BOOK NAME        |         AUTHORS                              | PRICE \n");
+                foreach (var x in booksArrary)
+                {
+                    if (x["book_Category_id"].Value<int>() == choice)
+                    {
+
+                        Console.WriteLine($"{i}    | {x["book_Name"].Value<string>()}                | {x["book_Author"].Value<string>()}                                   |  " + (n + 1000));
+                        i++;
+                    }
+                }
+                Console.WriteLine("\nChoose The Book Of Your Liking by Entering their ID's");
+                Console.WriteLine("                           OR ");
+                Console.WriteLine("                    Press 0 to go Back");
+                int myth1;
+                myth1 = int.Parse(Console.ReadLine());
+
+            }
+        }
+          
+    }
+}
---
+++ bookStore/bookStore/Register.cs
@@ -0,0 +1,54 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace bookStore
+{
+     class Register 
+    {
+       
+        public class Register_first
+        {
+
+            public string customer_Name { get; set; }
+            public string customer_Email { get; set; }
+            public int customer_Age { get; set; }
+            public string customer_Phone{ get; set; }
+            public string customer_Location { get; set; }
+            public string customer_Password { get; set; }
+
+        }
+
+       
+        public void get_Customer_Registered()
+        {
+            Console.Write("Please enter your Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Please enter your Email: ");
+            string email = Console.ReadLine();
+            Console.Write("Please enter your Age: ");
+            int age = int.Parse(Console.ReadLine());
+            Console.Write("Please enter your Phone: ");
+            string phone = Console.ReadLine();
+            Console.Write("Please enter your Location: ");
+            string location = Console.ReadLine();
+            Console.Write("Please enter your Password: ");
+            string password = Console.ReadLine();
+            var addNewCustomer = "{ 'customer_Name': '" + name + "', 'customer_Email': '" + email + "','customer_Age': '" + age + "','customer_Phone': '" + phone + "','customer_Location': '" + location + "','customer_Password': '" + password + "'}";
+            var jsonString = File.ReadAllText(@"customer_List.json");
+            var jsonObj = JObject.Parse(jsonString);
+            var customerArray = jsonObj.GetValue("customer") as JArray;
+            var newCustomer = JObject.Parse(addNewCustomer);
+            customerArray.Add(newCustomer);
+            jsonObj["customer"] = customerArray;
+            string newJsonResult = JsonConvert.SerializeObject(jsonObj,Formatting.Indented);
+            File.WriteAllText(@"customer_List.json", newJsonResult);
+            Console.WriteLine("You are successfully Registered with us");
+        }
+
+     
+    }
+}
